7f4b17357846029231fec3f6066129dbb9a7cc47
karginator <maksim.kargin@gmail.com>
2022-02-05 12:36:03 +0700

    autoresizing mask, added ProfileView.swift, added IBOoutlet for image labels and Test View

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Maksim Kargin on 05.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var Avatar: UIImageView!
+    
+    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var Birthday: UILabel!
+    
+    @IBOutlet weak var City: UILabel!
+    
+    @IBOutlet weak var Info: UITextView!
+}

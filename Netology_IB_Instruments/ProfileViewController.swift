8818b9ad2ade2de0bad6fe9e17c0f2ae3229cf08
karginator <maksim.kargin@gmail.com>
2022-02-05 14:17:17 +0700

    last commit for release

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -11,6 +11,13 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let profileViewName = String(describing: ProfileView.self)
+
+            if let profileView = Bundle.main.loadNibNamed(profileViewName, owner: nil, options: nil)?.first as? ProfileView {
+              profileView.frame = CGRect(x: 0/*15*/, y: 0/*100*/, width: view.bounds.width - 30, height: 350)
+              view.addSubview(profileView)
+            }
 
         // Do any additional setup after loading the view.
     }
@@ -25,5 +32,16 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    /*
 
+    let profileViewName = String(describing: ProfileView.self)
+
+        if let profileView = Bundle.main.loadNibNamed(profileViewName, owner: nil, options: nil)?.first as? ProfileView {
+          profileView.frame = CGRect(x: 15, y: 100, width: view.bounds.width - 30, height: 350)
+          view.addSubview(profileView)
+        }
+    
+     */
+    
 }
